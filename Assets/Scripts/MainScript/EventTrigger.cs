08d7cd8bc7e69f267bd75a6b2248408c0ab18e70
Due <kcy7th@naver.com>
2025-02-20 02:49:45 +0900

    feat : EventTrigger

---
+++ Assets/Scripts/MainScript/EventTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour
+{
+    public GameObject eventPanel; // 이벤트 판넬
+    private bool hasTriggered = false; // 중복 실행 방지
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !hasTriggered)
+        {
+            hasTriggered = true;
+            eventPanel.SetActive(true); 
+            Invoke(nameof(HideEvent), 3f); 
+        }
+    }
+
+    void HideEvent()
+    {
+        if (eventPanel != null)
+        {
+            eventPanel.SetActive(false);
+        }
+    }
+}
